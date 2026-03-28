c165b0a5c0bd2bea4146cc052d66ac3d757d11d7
Hektor Misplon <thibault.van.hagen@outlook.com>
2026-03-28 20:19:17 +0100

    feat: add new packages (darktable, krita, libreoffice, vscode, godot, etc.)

--- home/hosts/packages.nix
+++ home/hosts/packages.nix
@@ -4,20 +4,32 @@ with pkgs;
 [
   bat
   curl
+  networkmanager
+  darktable
   entr
   fd
   feh
   fzf
   htop
   jq
+  krita
+  libreoffice
+  nixfmt-rfc-style
   nmap
   nvimpager
   parallel
   pass
   ripgrep
+  setxkbmap
+  silver-searcher
   sops
+  sshfs
+  # stremio
   tldr
   tree
   unzip
+  vimPlugins.vim-plug
+  vscode
   wget
+  godot_4
 ]
